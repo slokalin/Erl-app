7c019ef567fd1f588ca13ae7caab48402147178c
Stan Kelevra <kelevra@infernohere.com>
2023-04-12 22:45:08 +0300

    Add Predator

--- main.erl
+++ main.erl
@@ -8,7 +8,8 @@ init() ->
                         {3, "Bill", 28, male},
                         {4, "Kate", 22, female},
 			{5, "Pedic", 47, male},
-                        {6, "Ivan", 14, male}]),
+			{6, "Predator", 100, male},
+                        {7, "Ivan", 14, male}]),
     ok.
 select() ->
     MS = ets:fun2ms(fun({Id, Name, Age, Gender})
